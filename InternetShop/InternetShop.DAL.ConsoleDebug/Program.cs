679e170b6209bfd07a491c5dc497b63c0099ee5f
Kicha99 <55016007+Kicha99@users.noreply.github.com>
2022-01-07 15:12:23 +0300

    Create debug app and start impliment data source

---
+++ InternetShop/InternetShop.DAL.ConsoleDebug/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InternetShop.DAL.ConsoleDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ InternetShop/InternetShop.DAL/DataSource.cs
@@ -0,0 +1,14 @@
+﻿using InternetShop.DAL.Interfaces;
+using System;
+
+namespace InternetShop.DAL
+{
+    public class DataSource : IDataSource
+    {
+        private readonly InternetShopDBContext _dBContext;
+        public DataSource()
+        {
+            _dBContext = new InternetShopDBContext();
+        }
+    }
+}
