93558a23e8442057227bf3235b3104cdad149527
Kicha99 <55016007+Kicha99@users.noreply.github.com>
2022-01-07 15:05:33 +0300

    Create DTOs

---
+++ InternetShop/InternetShop.DAL.Interfaces/DTO/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop.DAL.Interfaces.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<CategoryDTO> Child { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ InternetShop/InternetShop.DAL.Interfaces/DTO/OrderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop.DAL.Interfaces.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public IEnumerable<ProductDTO> Products { get; set; }
+    }
+}
